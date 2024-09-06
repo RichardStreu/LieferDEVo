a43416293b702fc0a76c4a3e4aae2a8b9548b789
Richard Streu <rstreu13@icloud.com>
2024-09-07 01:38:48 +0200

    last commit for this session

---
+++ notes/notes.md
@@ -0,0 +1,95 @@
+DISHES
+
+
+Italien
+-- Pizza Magaritha
+-- Pizza Salami
+-- Pizza Hawai
+-- Pasta Carbonara
+-- Pasta Bolognese
+
+American
+-- Hamburger
+-- Cheeseburger
+-- Chicken Wings
+-- Mc n Cheease
+-- Pulled Pork
+
+Asian
+-- Rotes Thai Curry
+-- Bratnudeln
+-- Sushiplatte
+-- Ramen
+-- Frühlingsrollen
+
+Beilagen
+-- Bruscetta
+-- Pommes
+-- Süßkartoffelpommes
+-- Chili Cheese Nuggets
+-- Wakame
+
+Deserts
+-- Panachotta
+-- Tiramisu
+-- Brownies
+-- Churros
+-- Donuts
+-- Mochi
+
+
+
+
+
+ich gebe dir gleich eine Struktur. du müsstest darazs bitte eine json struktur bauen und die einzelnen punkte ggf. ergänzen.
+
+Angefangen soll das array schonmal foods heißen, dieses array speichert die einzelnen kategorien. diese kategorien sind auch wieder arrays und speichern die einzelnen gerichte. die gerichte sind dann objects.
+
+jedes object muss folgende keys enthalten. In den klammern hinter den keys gebe ich dir anweisungen was du bei den values beachten musst.
+
+name: (bitte prüfe auf korrekten namen)
+description: (bitte lasse dir eine kurze beschreibung einfallen. gern auch indem du ein paar zutatten nennst wie zB bei den pizzen. die beschreibung muss kurz und pregnant sein, so wie bei einer app wo man essen bestellen kann)
+price: (lasse dir einen markttypischen preis einfallen, den du in folgender syntax schreibst: 12.99)
+count: (setze diesen wert standard auf 1)
+imgSource: (lasse diesen value unbesetzt)
+
+
+
+
+ich gebe dir jetzt meine food liste zum anpassen:
+
+Italien
+-- Pizza Magaritha
+-- Pizza Salami
+-- Pizza Hawai
+-- Pasta Carbonara
+-- Pasta Bolognese
+
+American
+-- Hamburger
+-- Cheeseburger
+-- Chicken Wings
+-- Mc n Cheease
+-- Pulled Pork
+
+Asian
+-- Rotes Thai Curry
+-- Bratnudeln
+-- Sushiplatte
+-- Ramen
+-- Frühlingsrollen
+
+Beilagen
+-- Bruscetta
+-- Pommes
+-- Süßkartoffelpommes
+-- Chili Cheese Nuggets
+-- Wakame
+
+Deserts
+-- Panachotta
+-- Tiramisu
+-- Brownies
+-- Churros
+-- Donuts
+-- Mochi
---
+++ scripts/db.js
@@ -0,0 +1,210 @@
+let foods = [
+  [
+    "Italien",
+    [
+      {
+        "name": "Pizza Margherita",
+        "description": "Klassische Pizza mit frischem Mozzarella, Tomatensauce und Basilikum.",
+        "price": "8.99",
+        "count": 1,
+        "imgSource": "../assets/img/italien/pizza-margherita.jpg"
+      },
+      {
+        "name": "Pizza Salami",
+        "description": "Knusprige Pizza mit Tomatensauce, Mozzarella und würziger Salami.",
+        "price": "9.99",
+        "count": 1,
+        "imgSource": "../assets/img/italien/pizza-salami.jpg"
+      },
+      {
+        "name": "Pizza Hawai",
+        "description": "Pizza mit Schinken, Ananas und Tomatensauce, überbacken mit Mozzarella.",
+        "price": "10.99",
+        "count": 1,
+        "imgSource": "../assets/img/italien/pizza-hawaii.jpg"
+      },
+      {
+        "name": "Pasta Carbonara",
+        "description": "Pasta in cremiger Soße mit Speck und Parmesan.",
+        "price": "12.99",
+        "count": 1,
+        "imgSource": "../assets/img/italien/pasta-carbonara.jpg"
+      },
+      {
+        "name": "Pasta Bolognese",
+        "description": "Pasta mit traditioneller Bolognese-Sauce aus Rinderhack und Tomaten.",
+        "price": "11.99",
+        "count": 1,
+        "imgSource": "../assets/img/italien/pasta-bolognese.jpg"
+      }
+    ]
+  ],
+  [
+    "American",
+    [
+      {
+        "name": "Hamburger",
+        "description": "Klassischer Hamburger mit saftigem Rindfleisch-Patty, Salat und Tomate.",
+        "price": "7.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Cheeseburger",
+        "description": "Hamburger mit geschmolzenem Käse, Rindfleisch-Patty, Salat und Tomate.",
+        "price": "8.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Chicken Wings",
+        "description": "Knusprige Chicken Wings, serviert mit einer würzigen BBQ-Sauce.",
+        "price": "6.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Mac n Cheese",
+        "description": "Cremige Makkaroni in einer reichhaltigen Käsesoße.",
+        "price": "5.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Pulled Pork",
+        "description": "Zartes, langsam gegartes Schweinefleisch in BBQ-Sauce.",
+        "price": "9.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      }
+    ]
+  ],
+  [
+    "Asian",
+    [
+      {
+        "name": "Rotes Thai Curry",
+        "description": "Würziges rotes Curry mit Kokosmilch und Gemüse.",
+        "price": "11.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Bratnudeln",
+        "description": "Gebratene Nudeln mit Gemüse und einer leichten Sojasoße.",
+        "price": "7.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Sushiplatte",
+        "description": "Verschiedene Sushi-Variationen mit frischem Fisch und Reis.",
+        "price": "14.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Ramen",
+        "description": "Herzhafte Nudelsuppe mit Gemüse, Ei und einer würzigen Brühe.",
+        "price": "12.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Frühlingsrollen",
+        "description": "Knusprige Frühlingsrollen, gefüllt mit Gemüse und Glasnudeln.",
+        "price": "5.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      }
+    ]
+  ],
+  [
+    "Beilagen",
+    [
+      {
+        "name": "Bruschetta",
+        "description": "Knuspriges Brot mit Tomaten, Basilikum und Olivenöl.",
+        "price": "4.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Pommes",
+        "description": "Knusprige Pommes Frites, perfekt als Beilage.",
+        "price": "3.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Süßkartoffelpommes",
+        "description": "Knusprige Süßkartoffelpommes mit einem Hauch von Süße.",
+        "price": "4.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Chili Cheese Nuggets",
+        "description": "Würzige Nuggets mit Chili und Käsefüllung.",
+        "price": "5.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Wakame",
+        "description": "Leichter Algensalat mit Sesam und Sojasoße.",
+        "price": "6.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      }
+    ]
+  ],
+  [
+    "Deserts",
+    [
+      {
+        "name": "Panna Cotta",
+        "description": "Cremiges Dessert aus Sahne mit einem Hauch von Vanille.",
+        "price": "4.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Tiramisu",
+        "description": "Klassisches italienisches Dessert mit Mascarpone und Espresso.",
+        "price": "5.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Brownies",
+        "description": "Saftige Schokoladenbrownies mit knackiger Kruste.",
+        "price": "3.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Churros",
+        "description": "Frittierte Teigstangen, serviert mit Zimt und Zucker.",
+        "price": "4.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Donuts",
+        "description": "Fluffige Donuts mit einer süßen Glasur.",
+        "price": "2.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      },
+      {
+        "name": "Mochi",
+        "description": "Weiche Reisküchlein mit süßer Füllung.",
+        "price": "5.99",
+        "count": 1,
+        "imgSource": "../assets/img/"
+      }
+    ]
+  ]
+];
+
