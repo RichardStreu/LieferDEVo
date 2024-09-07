022d0b4cbcc3099e25d14a1f6e2629d5d91df89b
Richard Streu <rstreu13@icloud.com>
2024-09-07 11:48:24 +0200

    array and img ready

--- scripts/db.js
+++ scripts/db.js
@@ -1,210 +1,197 @@
-let foods = [
-  [
-    "Italien",
-    [
-      {
-        "name": "Pizza Margherita",
-        "description": "Klassische Pizza mit frischem Mozzarella, Tomatensauce und Basilikum.",
-        "price": "8.99",
-        "count": 1,
-        "imgSource": "../assets/img/italien/pizza-margherita.jpg"
-      },
-      {
-        "name": "Pizza Salami",
-        "description": "Knusprige Pizza mit Tomatensauce, Mozzarella und würziger Salami.",
-        "price": "9.99",
-        "count": 1,
-        "imgSource": "../assets/img/italien/pizza-salami.jpg"
-      },
-      {
-        "name": "Pizza Hawai",
-        "description": "Pizza mit Schinken, Ananas und Tomatensauce, überbacken mit Mozzarella.",
-        "price": "10.99",
-        "count": 1,
-        "imgSource": "../assets/img/italien/pizza-hawaii.jpg"
-      },
-      {
-        "name": "Pasta Carbonara",
-        "description": "Pasta in cremiger Soße mit Speck und Parmesan.",
-        "price": "12.99",
-        "count": 1,
-        "imgSource": "../assets/img/italien/pasta-carbonara.jpg"
-      },
-      {
-        "name": "Pasta Bolognese",
-        "description": "Pasta mit traditioneller Bolognese-Sauce aus Rinderhack und Tomaten.",
-        "price": "11.99",
-        "count": 1,
-        "imgSource": "../assets/img/italien/pasta-bolognese.jpg"
-      }
-    ]
+
+let foods = {
+  "Italien": [
+    {
+      "name": "Pizza Margherita",
+      "description": "Klassische Pizza mit frischem Mozzarella, Tomatensauce und Basilikum.",
+      "price": "8.99",
+      "count": 1,
+      "imgSource": "../assets/img/italien/pizza-margherita.jpg"
+    },
+    {
+      "name": "Pizza Salami",
+      "description": "Knusprige Pizza mit Tomatensauce, Mozzarella und würziger Salami.",
+      "price": "9.99",
+      "count": 1,
+      "imgSource": "../assets/img/italien/pizza-salami.jpg"
+    },
+    {
+      "name": "Pizza Hawai",
+      "description": "Pizza mit Schinken, Ananas und Tomatensauce, überbacken mit Mozzarella.",
+      "price": "10.99",
+      "count": 1,
+      "imgSource": "../assets/img/italien/pizza-hawaii.jpg"
+    },
+    {
+      "name": "Pasta Carbonara",
+      "description": "Pasta in cremiger Soße mit Speck und Parmesan.",
+      "price": "12.99",
+      "count": 1,
+      "imgSource": "../assets/img/italien/pasta-carbonara.jpg"
+    },
+    {
+      "name": "Pasta Bolognese",
+      "description": "Pasta mit traditioneller Bolognese-Sauce aus Rinderhack und Tomaten.",
+      "price": "11.99",
+      "count": 1,
+      "imgSource": "../assets/img/italien/pasta-bolognese.jpg"
+    }
   ],
-  [
-    "American",
-    [
-      {
-        "name": "Hamburger",
-        "description": "Klassischer Hamburger mit saftigem Rindfleisch-Patty, Salat und Tomate.",
-        "price": "7.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Cheeseburger",
-        "description": "Hamburger mit geschmolzenem Käse, Rindfleisch-Patty, Salat und Tomate.",
-        "price": "8.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Chicken Wings",
-        "description": "Knusprige Chicken Wings, serviert mit einer würzigen BBQ-Sauce.",
-        "price": "6.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Mac n Cheese",
-        "description": "Cremige Makkaroni in einer reichhaltigen Käsesoße.",
-        "price": "5.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Pulled Pork",
-        "description": "Zartes, langsam gegartes Schweinefleisch in BBQ-Sauce.",
-        "price": "9.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      }
-    ]
+  "American": [
+    {
+      "name": "Hamburger",
+      "description": "Klassischer Hamburger mit saftigem Rindfleisch-Patty, Salat und Tomate.",
+      "price": "7.99",
+      "count": 1,
+      "imgSource": "../assets/img/american/hamburger.jpg"
+    },
+    {
+      "name": "Cheeseburger",
+      "description": "Hamburger mit geschmolzenem Käse, Rindfleisch-Patty, Salat und Tomate.",
+      "price": "8.99",
+      "count": 1,
+      "imgSource": "../assets/img/american/cheeseburger.jpg"
+    },
+    {
+      "name": "Chicken Wings",
+      "description": "Knusprige Chicken Wings, serviert mit einer würzigen BBQ-Sauce.",
+      "price": "6.99",
+      "count": 1,
+      "imgSource": "../assets/img/american/chicken-wings.jpg"
+    },
+    {
+      "name": "Mac n Cheese",
+      "description": "Cremige Makkaroni in einer reichhaltigen Käsesoße.",
+      "price": "5.99",
+      "count": 1,
+      "imgSource": "../assets/img/american/mac-n-cheese.jpg"
+    },
+    {
+      "name": "Pulled Pork",
+      "description": "Zartes, langsam gegartes Schweinefleisch in BBQ-Sauce.",
+      "price": "9.99",
+      "count": 1,
+      "imgSource": "../assets/img/american/pulled-pork.jpg"
+    }
   ],
-  [
-    "Asian",
-    [
-      {
-        "name": "Rotes Thai Curry",
-        "description": "Würziges rotes Curry mit Kokosmilch und Gemüse.",
-        "price": "11.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Bratnudeln",
-        "description": "Gebratene Nudeln mit Gemüse und einer leichten Sojasoße.",
-        "price": "7.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Sushiplatte",
-        "description": "Verschiedene Sushi-Variationen mit frischem Fisch und Reis.",
-        "price": "14.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Ramen",
-        "description": "Herzhafte Nudelsuppe mit Gemüse, Ei und einer würzigen Brühe.",
-        "price": "12.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Frühlingsrollen",
-        "description": "Knusprige Frühlingsrollen, gefüllt mit Gemüse und Glasnudeln.",
-        "price": "5.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      }
-    ]
+  "Asian": [
+    {
+      "name": "Rotes Thai Curry",
+      "description": "Würziges rotes Curry mit Kokosmilch und Gemüse.",
+      "price": "11.99",
+      "count": 1,
+      "imgSource": "../assets/img/asian/rotes-thai-curry.jpg"
+    },
+    {
+      "name": "Bratnudeln",
+      "description": "Gebratene Nudeln mit Gemüse und einer leichten Sojasoße.",
+      "price": "7.99",
+      "count": 1,
+      "imgSource": "../assets/img/asian/bratnudeln.jpg"
+    },
+    {
+      "name": "Sushiplatte",
+      "description": "Verschiedene Sushi-Variationen mit frischem Fisch und Reis.",
+      "price": "14.99",
+      "count": 1,
+      "imgSource": "../assets/img/asian/sushiplatte.jpg"
+    },
+    {
+      "name": "Ramen",
+      "description": "Herzhafte Nudelsuppe mit Gemüse, Ei und einer würzigen Brühe.",
+      "price": "12.99",
+      "count": 1,
+      "imgSource": "../assets/img/asian/ramen.jpg"
+    },
+    {
+      "name": "Frühlingsrollen",
+      "description": "Knusprige Frühlingsrollen, gefüllt mit Gemüse und Glasnudeln.",
+      "price": "5.99",
+      "count": 1,
+      "imgSource": "../assets/img/asian/fruehlingsrollen.jpg"
+    }
   ],
-  [
-    "Beilagen",
-    [
-      {
-        "name": "Bruschetta",
-        "description": "Knuspriges Brot mit Tomaten, Basilikum und Olivenöl.",
-        "price": "4.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Pommes",
-        "description": "Knusprige Pommes Frites, perfekt als Beilage.",
-        "price": "3.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Süßkartoffelpommes",
-        "description": "Knusprige Süßkartoffelpommes mit einem Hauch von Süße.",
-        "price": "4.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Chili Cheese Nuggets",
-        "description": "Würzige Nuggets mit Chili und Käsefüllung.",
-        "price": "5.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Wakame",
-        "description": "Leichter Algensalat mit Sesam und Sojasoße.",
-        "price": "6.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      }
-    ]
+  "Beilagen": [
+    {
+      "name": "Bruschetta",
+      "description": "Knuspriges Brot mit Tomaten, Basilikum und Olivenöl.",
+      "price": "4.99",
+      "count": 1,
+      "imgSource": "../assets/img/beilagen/bruschetta.jpg"
+    },
+    {
+      "name": "Pommes",
+      "description": "Knusprige Pommes Frites, perfekt als Beilage.",
+      "price": "3.99",
+      "count": 1,
+      "imgSource": "../assets/img/beilagen/pommes.jpg"
+    },
+    {
+      "name": "Süßkartoffelpommes",
+      "description": "Knusprige Süßkartoffelpommes mit einem Hauch von Süße.",
+      "price": "4.99",
+      "count": 1,
+      "imgSource": "../assets/img/beilagen/suesskartoffelpommes.jpg"
+    },
+    {
+      "name": "Chili Cheese Nuggets",
+      "description": "Würzige Nuggets mit Chili und Käsefüllung.",
+      "price": "5.99",
+      "count": 1,
+      "imgSource": "../assets/img/beilagen/chili-cheese-nuggets.jpg"
+    },
+    {
+      "name": "Wakame",
+      "description": "Leichter Algensalat mit Sesam und Sojasoße.",
+      "price": "6.99",
+      "count": 1,
+      "imgSource": "../assets/img/beilagen/wakame.jpg"
+    }
   ],
-  [
-    "Deserts",
-    [
-      {
-        "name": "Panna Cotta",
-        "description": "Cremiges Dessert aus Sahne mit einem Hauch von Vanille.",
-        "price": "4.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Tiramisu",
-        "description": "Klassisches italienisches Dessert mit Mascarpone und Espresso.",
-        "price": "5.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Brownies",
-        "description": "Saftige Schokoladenbrownies mit knackiger Kruste.",
-        "price": "3.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Churros",
-        "description": "Frittierte Teigstangen, serviert mit Zimt und Zucker.",
-        "price": "4.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Donuts",
-        "description": "Fluffige Donuts mit einer süßen Glasur.",
-        "price": "2.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      },
-      {
-        "name": "Mochi",
-        "description": "Weiche Reisküchlein mit süßer Füllung.",
-        "price": "5.99",
-        "count": 1,
-        "imgSource": "../assets/img/"
-      }
-    ]
+  "Deserts": [
+    {
+      "name": "Panna Cotta",
+      "description": "Cremiges Dessert aus Sahne mit einem Hauch von Vanille.",
+      "price": "4.99",
+      "count": 1,
+      "imgSource": "../assets/img/deserts/panna-cotta.jpg"
+    },
+    {
+      "name": "Tiramisu",
+      "description": "Klassisches italienisches Dessert mit Mascarpone und Espresso.",
+      "price": "5.99",
+      "count": 1,
+      "imgSource": "../assets/img/deserts/tiramisu.jpg"
+    },
+    {
+      "name": "Brownies",
+      "description": "Saftige Schokoladenbrownies mit knackiger Kruste.",
+      "price": "3.99",
+      "count": 1,
+      "imgSource": "../assets/img/deserts/brownies.jpg"
+    },
+    {
+      "name": "Churros",
+      "description": "Frittierte Teigstangen, serviert mit Zimt und Zucker.",
+      "price": "4.99",
+      "count": 1,
+      "imgSource": "../assets/img/deserts/churros.jpg"
+    },
+    {
+      "name": "Donuts",
+      "description": "Fluffige Donuts mit einer süßen Glasur.",
+      "price": "2.99",
+      "count": 1,
+      "imgSource": "../assets/img/deserts/donuts.jpg"
+    },
+    {
+      "name": "Mochi",
+      "description": "Weiche Reisküchlein mit süßer Füllung.",
+      "price": "5.99",
+      "count": 1,
+      "imgSource": "../assets/img/deserts/mochi.jpg"
+    }
   ]
-];
+};
+
 
